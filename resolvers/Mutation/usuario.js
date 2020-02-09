b221389760cdda4609364826ea04945784a4b1b3
developersloop <loopinfinite.all@gmail.com>
2020-02-09 11:42:31 -0300

    .

--- resolvers/Mutation/usuario.js
+++ resolvers/Mutation/usuario.js
@@ -2,7 +2,38 @@ const db = require('../../config/db')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+           const { nome,email,senha,...perfis } = dados;
+
+           let newUser = {
+                     nome,
+                     email,
+                     senha
+           }
+
+           let verifyEmail = await db('usuarios')
+                             .where({email,})
+                             .count();
+
+            if(verifyEmail > 0){
+                   throw new Error('Este email já esta em nossa base de dados!');
+            }
+
+            // let lastIdUser = await db('usuarios').insert(newUser);
+            let lastIdPerfil = await db('perfis').insert(perfis);
+
+            perfis.forEach(element => {
+                console.log(element);    
+            });
+            // console.log(lastIdPerfil);
+
+            // let user = await db('usuarios')
+            //             .where({'id':lastIdUser})
+            //             .select('id',
+            //                     'nome',
+            //                     'email')
+            //             .first();
+            
+            console.log(user);
     },
     async excluirUsuario(_, { filtro }) {
         // Implementar
